a774557dda5e2db9e9428cb4bf44f5f995963fea
Huang <huang37@mail2000.com.tw>
2018-03-02 23:18:58 +0800

    Add bmi file

---
+++ bmi.py
@@ -0,0 +1,19 @@
+height = input('請輸入身高（cm）:')
+weight = input('請輸入體重（kg）:')
+height = int(height)
+weight = int(weight)
+height = height / 100
+bmi = weight / height / height
+
+if bmi < 18.5:
+  print('你的BMI值為：',bmi, '體重過輕')
+elif bmi >= 18.5 and bmi < 24:
+  print('你的BMI值為：',bmi, '體重正常')
+elif bmi >= 24 and bmi < 27:
+  print('你的BMI值為：',bmi, '稍重')
+elif bmi >= 27 and bmi < 30:
+  print('你的BMI值為：',bmi, '輕度肥胖')
+elif bmi >= 30 and bmi < 35:
+  print('你的BMI值為：',bmi, '中度肥胖')
+else:
+  print('你的BMI值為：',bmi, '重度肥胖')
